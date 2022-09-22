a6219a7b4d9bff55efea077ac59d33670f8a5eb7
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2022-09-22 17:50:15 +0900

    Python_Matplotlib_Study

--- Study_Python_Jupyter/조합.py
+++ Study_Python_Jupyter/조합.py
@@ -1,14 +1,26 @@
 
-sample = ["a", "b", "c"]
 
-# def YesDuple(list):
 
-element = []
-all = []
-for a in sample:
-    for b in sample:
-        for c in sample:
-            to_list = a+b+c+""
+def YesDuple(sample):
+    
+    all = [] #묶음 집합
+
+    num = 0
+    to_list = ""
+    while True:
+        for a in sample[num: ]:
+            to_list = to_list + a 
             all.append(list(to_list))
+            num += 1
+        
+        if len(sample) == num:
+            break
+
+    return all
+
+test = [1,2,3]
+YesDuple(test)
+
             
-print(all)
+
+
